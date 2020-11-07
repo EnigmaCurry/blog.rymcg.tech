1115a7f6417f5324c4e930e8db618c4b1090629e
EnigmaCurry <ryan@enigmacurry.com>
2020-11-06 18:39:20 -0800

    gitea

--- src/k3s/gitea/env.sh
+++ src/k3s/gitea/env.sh
@@ -1,17 +1,30 @@
 ### gitea environment
 ## ALL_VARS is the names of all of variables passed to the templates:
-export ALL_VARS=(POSTGRES_PVC_SIZE POSTGRES_PORT)
+export ALL_VARS=(IMAGE PVC_SIZE POSTGRES_PVC_SIZE DOMAIN APP_NAME SSH_PORT \
+                DISABLE_REGISTRATION REQUIRE_SIGNIN_VIEW)
 
 ## All secrets are input at *render* time, and made into a sealed secret.
 ## The name of the sealed secret to create:
 export SECRET=gitea
 ## List of the *names* of all of the secret variables to ask for:
-export ALL_SECRETS=(postgres_user postgres_password)
+export ALL_SECRETS=(secret_key postgres_user postgres_password)
 
-## Size of database:
+## Container image
+export IMAGE=gitea/gitea:latest
+## Domain name for UI and clone URLs
+export DOMAIN="git.k3s.example.com"
+## UI title
+export APP_NAME="${DOMAIN}"
+## SSH PORT (external; from traefik)
+export SSH_PORT="2222"
+## Enable or disable registration:
+export DISABLE_REGISTRATION="true"
+## Require sign-in
+export REQUIRE_SIGNIN_VIEW="true"
+## Size of data volume:
+export PVC_SIZE=5Gi
+## Size of database volume:
 export POSTGRES_PVC_SIZE=5Gi
-## TCP port
-export POSTGRES_PORT=5432
 
 ## Default template source directory, or http path:
 TEMPLATE_SRC=${TEMPLATE_SRC:-https://raw.githubusercontent.com/EnigmaCurry/blog.rymcg.tech/master/src/k3s}
@@ -19,4 +32,6 @@ TEMPLATE_SRC=${TEMPLATE_SRC:-https://raw.githubusercontent.com/EnigmaCurry/blog.
 export TEMPLATES=(
     $TEMPLATE_SRC/gitea/gitea.postgres.pvc.template.yaml
     $TEMPLATE_SRC/gitea/gitea.postgres.template.yaml
+    $TEMPLATE_SRC/gitea/gitea.pvc.template.yaml
+    $TEMPLATE_SRC/gitea/gitea.template.yaml
 )
