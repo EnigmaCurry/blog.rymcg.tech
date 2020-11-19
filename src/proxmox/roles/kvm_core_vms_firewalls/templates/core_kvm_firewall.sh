f037b304bcf9318a30bae323b65d0596b846ac09
EnigmaCurry <ryan@enigmacurry.com>
2020-11-19 14:58:19 -0800

    proxmox

--- src/proxmox/roles/kvm_core_vms_firewalls/templates/core_kvm_firewall.sh
+++ src/proxmox/roles/kvm_core_vms_firewalls/templates/core_kvm_firewall.sh
@@ -1,6 +1,8 @@
 source /etc/firewall_helpers.sh
 
 port_forward 2222 10.10.1.101 22
+port_forward 2223 10.10.2.101 22
+port_forward 2224 10.10.2.102 22
 
 {% for public_ip in proxmox_public_ips %}
 port_open_tcp {{ proxmox_public_interface }} {{ public_ip }} 2222
