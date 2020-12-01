f0ff998e562b712ca67db0db419a6223fdbc3621
EnigmaCurry <ryan@enigmacurry.com>
2020-12-01 14:31:33 -0800

    registry

---
+++ content/blog/registry.md
@@ -0,0 +1,13 @@
+---
+title: "k3s part3: Container Registry"
+url: "blog/registry"
+date: 2020-12-01T13:32:03-07:00
+tags: ['k3s', 'kubernetes']
+---
+
+## Links
+
+https://github.com/alexellis/k8s-tls-registry
+https://rancher.com/docs/k3s/latest/en/installation/private-registry/
+https://doc.traefik.io/traefik/https/tls/#client-authentication-mtls
+
---
+++ src/k3s/registry/env.sh
@@ -0,0 +1,35 @@
+### gitea environment
+## ALL_VARS is the names of all of variables passed to the templates:
+export ALL_VARS=(IMAGE)
+
+export IMAGE=registry:2
+
+## All secrets are input at *render* time, and made into sealed secret(s).
+export SECRET="registry"
+## List of the *names* of all of the secret variables to ask for:
+## Variables not defined are interactively asked for.
+## Variables that are defined, are used as-is.
+export ALL_SECRETS=(HTPASSWD)
+
+
+## Generate passwords:
+if [[ ! -f registry.sealed_secret.yaml ]]; then
+    SHA=$(head -c 16 /dev/urandom | shasum | cut -d " " -f 1)
+    echo "Generated password (save this): ${SHA}"
+    export HTPASSWD=$(kubectl run --quiet -i --rm --tty htpasswd-gen-$RANDOM --image=alpine --restart=Never -- /bin/sh -c "apk add --no-cache apache2-utils &> /dev/null && htpasswd -Bbn admin ${SHA} | head -n 1 2> /dev/null")
+    if [[ $(echo $HTPASSWD | wc -c) -lt 50 ]]; then
+        echo "Error generating HTPASSWD"
+        exit 1
+    fi
+fi
+
+## Default template source directory, or http path:
+## TEMPLATE_SRC=${TEMPLATE_SRC:-https://raw.githubusercontent.com/EnigmaCurry/blog.rymcg.tech/master/src/k3s}
+TEMPLATE_SRC=$(pwd)
+
+## YAML Template locations (can be file paths or https:// URLs)
+export TEMPLATES=(
+    $TEMPLATE_SRC/registry/registry.template.yaml
+)
+## Secret Templates
+export SECRET_TEMPLATES=()
