a842c49d55bf2202015dc73fdda38ec35e076297
EnigmaCurry <ryan@enigmacurry.com>
2023-11-22 15:24:45 -0700

    proxmox_nat.sh

--- src/proxmox/proxmox_nat.sh
+++ src/proxmox/proxmox_nat.sh
@@ -6,10 +6,13 @@ IPTABLES_RULES_SCRIPT="/etc/network/${SYSTEMD_UNIT}.sh"
 
 set -eo pipefail
 stderr(){ echo "$@" >/dev/stderr; }
-error(){ echo "Error: $@" >/dev/stderr; }
-cancel(){ echo "Canceled." >/dev/stderr; exit 2; }
-fault(){ test -n "$1" && error $1; echo "Exiting." >/dev/stderr; exit 1; }
+error(){ stderr "Error: $@"; }
+cancel(){ stderr "Canceled."; exit 2; }
+fault(){ test -n "$1" && error $1; stderr "Exiting."; exit 1; }
 print_array(){ printf '%s\n' "$@"; }
+trim_trailing_whitespace() { sed -e 's/[[:space:]]*$//'; }
+trim_leading_whitespace() { sed -e 's/^[[:space:]]*//'; }
+trim_whitespace() { trim_leading_whitespace | trim_trailing_whitespace; }
 confirm() {
     ## Confirm with the user.
     local default=$1; local prompt=$2; local question=${3:-". Proceed?"}
@@ -33,6 +36,11 @@ ask() {
         export ${__var}
         [[ -z "${!__var}" ]] || break
     done
+}
+ask_allow_blank() {
+    local __prompt="${1}"; local __var="${2}"; local __default="${3}"
+    read -e -p "${__prompt}"$'\x0a\e[32m:\e[0m ' -i "${__default}" ${__var}
+    export ${__var}
 }
 check_var(){
     local __missing=false
@@ -63,7 +71,14 @@ get_bridges() {
     readarray -t INTERFACES < <(cat /etc/network/interfaces | grep -Po "^iface \K(vmbr[0-9]*)")
     stderr ""
     stderr "Currently configured bridges:"
-    print_array "${INTERFACES[@]}"
+    (
+        echo "BRIDGE|NETWORK|COMMENT
+"
+        for i in "${INTERFACES[@]}"; do
+            local COMMENT="$(get_interface_comment ${i})"
+            echo "${i}|x.x.x.x|$(get_interface_comment ${i})"
+        done
+    ) | column -t -s '|' | trim_trailing_whitespace
 }
 prefix_to_netmask () {
     #thanks https://forum.archive.openwrt.org/viewtopic.php?id=47986&p=1#p220781
@@ -91,12 +106,10 @@ debug_var() {
 }
 
 new_interface() {
-    local INTERFACE IP_CIDR IP_ADDRESS NETMASK COMMENT OTHER_BRIDGE
-    get_bridges
+    local INTERFACE IP_CIDR IP_ADDRESS NETMASK COMMENT OTHER_BRIDGE DEFAULT_IP_CIDR
     set -e
     echo
-    confirm no "Do you want to create a new NAT bridge" "?"  || return 1
-    echo
+    echo "Configuring new NAT bridge ..."
     ask "Enter the existing bridge to NAT from" OTHER_BRIDGE vmbr0
     if ! element_in_array "$OTHER_BRIDGE" "${INTERFACES[@]}"; then
         fault "Sorry, ${OTHER_BRIDGE} is not a valid bridge (it does not exist)"
@@ -106,13 +119,20 @@ new_interface() {
     INTERFACE="vmbr${BRIDGE_NUMBER}"
 
     if element_in_array "$INTERFACE" "${INTERFACES[@]}"; then
-        fault "Sorry, ${INTERFACE} already exists."
+        error "Sorry, ${INTERFACE} already exists."
+        echo
+        return
     fi
     echo
     echo "Configuring new interface: ${INTERFACE}"
-    ask "Enter the static IP address and network prefix in CIDR notation for ${INTERFACE}:" IP_CIDR "10.${BRIDGE_NUMBER}.0.1/24"
+    if [[ "${BRIDGE_NUMBER}" -ge 0 ]] && [[ "${BRIDGE_NUMBER}" -le 255 ]]; then
+        DEFAULT_IP_CIDR="10.${BRIDGE_NUMBER}.0.1/24"
+    else
+        DEFAULT_IP_CIDR=""
+    fi
+    ask "Enter the static IP address and network prefix in CIDR notation for ${INTERFACE}:" IP_CIDR "${DEFAULT_IP_CIDR}"
     if ! validate_ip_network "${IP_CIDR}"; then
-        fault "Bad IP address / network"
+        fault "Bad IP address/network prefix (use the format eg. 192.168.1.1/24)"
     fi
     echo
     debug_var IP_CIDR
@@ -126,7 +146,8 @@ new_interface() {
     fi
     debug_var IP_ADDRESS
     debug_var NETMASK
-    ask "Enter the description for this interface" COMMENT "NAT ${IP_CIDR} bridged to ${OTHER_BRIDGE}"
+    echo
+    ask "Enter the description/comment for this interface" COMMENT "NAT ${IP_CIDR} bridged to ${OTHER_BRIDGE}"
     cat <<EOF >> /etc/network/interfaces
 
 auto ${INTERFACE}
@@ -145,6 +166,10 @@ EOF
     echo "Wrote /etc/network/interfaces"
     ifup "${INTERFACE}"
     echo "Activated ${INTERFACE}"
+}
+
+get_interface_comment() {
+    awk "/^iface ${1} /,/^$/" /etc/network/interfaces | grep -v -e '^$' | grep -e '^#' | tail -1 | tr -d '#'
 }
 
 activate_iptables_rules() {
@@ -256,8 +281,8 @@ EOF
 
 ## Here is an example with two rules (commented out), and explained:
 ##  * For any TCP packet on port 2222 coming from vmbr0, forward to 10.15.0.2 on port 22
-##  * For any TCP or UDP packet on port 5353 coming from vmbr0, forward to 10.15.0.3 on port 53
-## PORT_FORWARD_RULES=(vmbr0:tcp:2222:10.15.0.2:22 vmbr0:any:5353:10.15.0.3:53)
+##  * For any UDP packet on port 5353 coming from vmbr0, forward to 10.15.0.3 on port 53
+## PORT_FORWARD_RULES=(vmbr0:tcp:2222:10.15.0.2:22 vmbr0:udp:5353:10.15.0.3:53)
 
 PORT_FORWARD_RULES=(${PORT_FORWARD_RULES[@]})
 
@@ -278,7 +303,7 @@ print_port_forward_rule() {
     DEST_IP="${rule_parts[3]}"
     DEST_PORT="${rule_parts[4]}"
     check_var INTERFACE PROTOCOL IN_PORT DEST_IP DEST_PORT
-    echo "${INTERFACE} ${PROTOCOL} ${IN_PORT} ${DEST_IP} ${DEST_PORT}"
+    echo "${INTERFACE}|${PROTOCOL}|${IN_PORT}|${DEST_IP}|${DEST_PORT}"
 }
 
 print_port_forward_rules() {
@@ -288,20 +313,20 @@ print_port_forward_rules() {
     else
         echo "## Existing inbound port forwarding (DNAT) rules:"
         (
-            echo "INTERFACE PROTOCOL IN_PORT DEST_IP DEST_PORT"
+            echo "INTERFACE|PROTOCOL|IN_PORT|DEST_IP|DEST_PORT"
             for rule in "${PORT_FORWARD_RULES[@]}"; do
                 print_port_forward_rule "${rule}"
             done
-        ) | column -t
+        ) | column -t -s '|'
     fi
 }
 
 define_port_forwarding_rules() {
     readarray -t PORT_FORWARD_RULES < <(get_port_forward_rules)
     while true; do
-        confirm no "Would you like to define new port forwarding rules" "?" || break
+        echo "Defining new port forward rule:"
         ask "Enter the inbound interface" INTERFACE vmbr0
-        ask "Enter the protocol (tcp, udp, any)" PROTOCOL tcp
+        ask "Enter the protocol (tcp, udp)" PROTOCOL tcp
         ask "Enter the inbound Port number" IN_PORT
         check_num IN_PORT
         ask "Enter the destination IP address" DEST_IP
@@ -311,11 +336,13 @@ define_port_forwarding_rules() {
         check_var INTERFACE PROTOCOL IN_PORT DEST_IP DEST_PORT
         local RULE="${INTERFACE}:${PROTOCOL}:${IN_PORT}:${DEST_IP}:${DEST_PORT}"
         (
-            echo "INTERFACE PROTOCOL IN_PORT DEST_IP DEST_PORT"
+            echo "INTERFACE|PROTOCOL|IN_PORT|DEST_IP|DEST_PORT"
             print_port_forward_rule "${RULE}"
-        ) | column -t
+        ) | column -t -s '|'
         confirm yes "Is this rule correct" "?" || return
         PORT_FORWARD_RULES+=("$RULE")
+        echo
+        confirm no "Would you like to define more port forwarding rules now" "?" || break
     done
     create_iptables_rules "${PORT_FORWARD_RULES[@]}"
     activate_iptables_rules
@@ -324,12 +351,84 @@ define_port_forwarding_rules() {
     echo
 }
 
+delete_port_forwarding_rules() {
+    readarray -t PORT_FORWARD_RULES < <(get_port_forward_rules)
+    while true; do
+        if [[ "${PORT_FORWARD_RULES[@]}" == "" ]]; then
+            print_port_forward_rules
+            break
+        fi
+        echo
+        (
+            echo "LINE# INTERFACE PROTOCOL IN_PORT DEST_IP DEST_PORT"
+            print_port_forward_rules 2>/dev/null | grep -v "#" | tail -n +2 | cat -n | trim_whitespace
+        ) | column -t | trim_whitespace
+        ask_allow_blank 'Enter the line number for the rule you wish to delete (type `q` or blank for none)' RULE_TO_DELETE
+        if [[ -z "${RULE_TO_DELETE}" ]] || [[ "${RULE_TO_DELETE}" == "q" ]]; then
+            break
+        fi
+        RULE_TO_DELETE=$((${RULE_TO_DELETE} - 1))
+        if [[ "${RULE_TO_DELETE}" -lt 0 ]] || \
+               [[ "${RULE_TO_DELETE}" -gt "${#PORT_FORWARD_RULES[@]}" ]]; then
+            error "Invalid rule number"
+            break
+        fi
+        local to_delete="${PORT_FORWARD_RULES[${RULE_TO_DELETE}]}"
+        PORT_FORWARD_RULES=("${PORT_FORWARD_RULES[@]/${to_delete}}")
+        create_iptables_rules "${PORT_FORWARD_RULES[@]}"
+        activate_iptables_rules
+    done
+    echo
+}
+
+print_help() {
+    echo "NAT bridge tool:"
+    echo ' * Type `i` or `interfaces` to list the bridge interfaces.'
+    echo ' * Type `c` or `create` to create a new NAT bridge.'
+    echo ' * Type `l` or `list` to list the NAT rules.'
+    echo ' * Type `n` or `new` to create some new NAT rules.'
+    echo ' * Type `d` or `delete` to delete some existing NAT rules.'
+    echo ' * Type `?` or `help` to see this help message again.'
+    echo ' * Type `q` or `quit` to quit.'
+}
+
 main() {
-    new_interface || true
+    # new_interface || true
+    # echo
+    # print_port_forward_rules
+    # echo
+    # define_port_forwarding_rules
+    # echo
+    # delete_port_forwarding_rules
+
     echo
-    print_port_forward_rules
+    get_bridges
     echo
-    define_port_forwarding_rules
+    while :
+    do
+        print_help
+        echo
+        ask_allow_blank 'Enter command (for help, enter `?`)' COMMAND
+        echo
+        if [[ "$COMMAND" == 'q' ]] || [[ "$COMMAND" == 'quit' ]]; then
+            echo "goodbye"
+            exit 0
+        elif [[ $COMMAND == '?' || $COMMAND == "help" ]]; then
+            print_help
+        elif [[ $COMMAND == "i" || $COMMAND == "interfaces" ]]; then
+            get_bridges
+        elif [[ $COMMAND == "c" || $COMMAND == "create" ]]; then
+            get_bridges
+            new_interface || true
+        elif [[ $COMMAND == "l" || $COMMAND == "list" ]]; then
+            print_port_forward_rules
+        elif [[ $COMMAND == "n" || $COMMAND == "new" ]]; then
+            define_port_forwarding_rules
+        elif [[ $COMMAND == "d" || $COMMAND == "delete" ]]; then
+            delete_port_forwarding_rules
+        fi
+        echo
+    done
 }
 
 main
